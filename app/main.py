460fbec3bd2cfc6fed8aae0cf0253862ec13417d
Abarna <abarnaragsitha08@gmail.com>
2026-08-01 11:08:19 +0530

    Created User model and connected MySQL database

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 
+from app.database.database import Base, engine
+from app.models.user import User
+
+# Create all database tables
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(
     title="Blood Donation Network & Emergency Matching Platform",
     version="1.0.0",
     description="REST API for Blood Donation Platform"
 )
+
 
 @app.get("/")
 def home():
