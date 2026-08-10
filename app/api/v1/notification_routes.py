509d62e2b0936fb56f1402c098bd0d3ca79203c1
Abarna <abarnaragsitha08@gmail.com>
2026-08-10 11:49:18 +0530

    Implement notification read status API

--- app/api/v1/notification_routes.py
+++ app/api/v1/notification_routes.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-
+from app.core.security import get_current_user
 from app.database.database import get_db
 from app.services.notification_service import create_notification
-
+from app.services.notification_service import get_user_notifications
+from app.services.notification_service import mark_notification_as_read
 router = APIRouter(
     prefix="/api/v1/notifications",
     tags=["Notifications"]
@@ -21,5 +22,25 @@ def create(
         user_id,
         title,
         message,
+        db
+    )
+@router.get("/")
+def get_notifications(
+    current_user=Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    return get_user_notifications(
+        current_user.id,
+        db
+    )
+@router.put("/{notification_id}/read")
+def mark_as_read(
+    notification_id: int,
+    current_user=Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    return mark_notification_as_read(
+        notification_id,
+        current_user.id,
         db
     )
--- app/services/notification_service.py
+++ app/services/notification_service.py
@@ -30,3 +30,55 @@ def create_notification(
             "status": notification.status
         }
     }
+def get_user_notifications(user_id: int, db: Session):
+
+    notifications = db.query(Notification).filter(
+        Notification.user_id == user_id
+    ).all()
+
+    return {
+        "success": True,
+        "message": "Notifications fetched successfully",
+        "data": [
+            {
+                "id": notification.id,
+                "title": notification.title,
+                "message": notification.message,
+                "status": notification.status
+            }
+            for notification in notifications
+        ]
+    }
+def mark_notification_as_read(
+    notification_id: int,
+    user_id: int,
+    db: Session
+):
+
+    notification = db.query(Notification).filter(
+        Notification.id == notification_id,
+        Notification.user_id == user_id
+    ).first()
+
+    if notification is None:
+        return {
+            "success": False,
+            "message": "Notification not found",
+            "data": None
+        }
+
+    notification.status = "Read"
+
+    db.commit()
+    db.refresh(notification)
+
+    return {
+        "success": True,
+        "message": "Notification marked as read",
+        "data": {
+            "id": notification.id,
+            "title": notification.title,
+            "message": notification.message,
+            "status": notification.status
+        }
+    }
